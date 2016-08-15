c3e9fb85775db5e262b24d44e1f405468ba82bc1
Brian Behrens <brian.behrens@wearepop.com>
2016-08-15 14:23:55 -0700

    Update responsive header

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
       <div class="absolute-header">
         <header class="site-header" role="banner">
           <hgroup>
-            <h1 class="site-header--title"><span><a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></span></h1>
+            <h1 class="site-header--title"><a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
             <h2 class="site-header--description"><?php bloginfo('description'); ?></h2>
           </hgroup>
           <nav class="site-navigation" role="navigation">
